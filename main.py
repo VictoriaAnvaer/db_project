f9ebdf4a174950f10f112e76b3a432af556fd705
VictoriaAnvaer <145706598+VictoriaAnvaer@users.noreply.github.com>
2025-05-13 12:57:29 -0400

    Add files via upload
    initial

---
+++ main.py
@@ -0,0 +1,39 @@
+import mysql.connector
+
+
+def get_db_connection():
+    connection = mysql.connector.connect(user='victoriaa127',
+                                         password='230826075',
+                                         host='10.8.37.226',
+                                         database='victoriaa127_db')
+    return connection
+
+
+def execute_query(connection, query):
+    cursor = connection.cursor()
+    cursor.execute(query)
+    results = []
+
+    for row in cursor:
+        results.append(row)
+
+    cursor.close()
+    connection.close()
+    return results
+
+
+def get_stud_schedule():
+    stud_id = input("Enter student ID: ")
+    return execute_query(get_db_connection(), "CALL select_student_schedule (" + stud_id + ");")
+
+
+results = get_stud_schedule()
+
+for row in results:
+    print("")
+    print("Period: " + str(row[1]))
+    print("Course: " + row[2])
+    print("Room: " + row[3])
+    print("Teacher: " + row[4])
+
+
